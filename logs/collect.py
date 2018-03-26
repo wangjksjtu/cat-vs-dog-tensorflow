f2160f24da1e5352759e050ead1c7c631d1e697b
wangjksjtu <wangjksjtu_01@sjtu.edu.cn>
2018-03-26 21:16:58 +0800

    Wrap the collect.py using argparse

--- logs/collect.py
+++ logs/collect.py
@@ -1,6 +1,13 @@
 import os
+import argparse
 
-log_path = "quality_0/train/log_train.txt"
+parser = argparse.ArgumentParser()
+parser.add_argument('--quality', type=int, default=0, help='Image quality [default: 0]')
+FLAGS = parser.parse_args()
+
+QUALITY = FLAGS.quality
+
+log_path = "quality_" + str(QUALITY) + "/train/log_train.txt"
 k = 10
 
 def max_acc(log_path):
@@ -16,8 +23,9 @@ def top_k_avg_cls_acc(log_path, k):
     os.system("cat %s | grep eval\ avg | awk '{print $5}' | sort | tail -n %s | awk '{ sum += $0 } END { print sum / NR }'" % (log_path, str(k)))
 
 if __name__ == "__main__":
-    max_acc(log_path)
-    # top_k_acc(log_path, k)
+    print log_path
+    # max_acc(log_path)
+    top_k_acc(log_path, k)
     # max_avg_cls_acc(log_path)
     # top_k_avg_cls_acc(log_path, k)
 
