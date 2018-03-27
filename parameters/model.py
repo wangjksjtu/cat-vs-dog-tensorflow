d379676c2db034681618d56ecb3421a3c0d9f87a
wangjksjtu <wangjksjtu_01@sjtu.edu.cn>
2018-03-27 20:06:14 +0800

    Add ten models for parameter minimization

--- parameters/model.py
+++ parameters/model.py
@@ -1,45 +1,160 @@
+import argparse
+import os
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
 from keras.optimizers import Adam
 from utils import LRN2D
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--setting', type=int, default=8, help='Model architecture (0-8) [default: 8]')
+parser.add_argument('--model', type=str, default="", help='Model architecture description (0-8) [default: ""]')
+parser.add_argument('--all', type=bool, default=False, help="Print all architecures [default: False]")
+parser.add_argument('--save_dir', type=str, default="models", help="The output directory path [default: models/]")
+FLAGS = parser.parse_args()
+
 nb_classes = 2
 img_rows, img_cols = 208, 208
-batch_size = 128
 
-model = Sequential()
-model.add(Conv2D(16, (3, 3), strides=(1, 1), padding='same',
-                 activation='relu', input_shape=(208,208,3)))
-model.add(MaxPooling2D((3,3), (2, 2), padding='same'))
-# model.add(BatchNormalization())
-model.add(LRN2D())
+SETTING = FLAGS.setting
+MODEL = FLAGS.model
+ALL = FLAGS.all
+SAVE_DIR = FLAGS.save_dir
 
-'''
-model.add(Conv2D(16, (3, 3), strides=(1, 1), padding='same',
-                 activation='relu'))
-model.add(MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
-# model.add(BatchNormalization())
-model.add(LRN2D())
-'''
+if not os.path.exists(SAVE_DIR):
+    os.makedirs(SAVE_DIR)
 
-model.add(Conv2D(16, (3, 3), strides=(1, 1), padding='same',
-                 activation='relu'))
-model.add(MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
-model.add(LRN2D())
-# model.add(BatchNormalization())
+model_list = ["io-units_x1+cnn_pooling2",
+              "io-units_x1+cnn_pooling",
+              "io-units_x2",
+              "io-layer",
+              "io-units_x1",
+              "io-units_x1+layer",
+              "io-units_x1+units_x1",
+              "io-units_x1+units_x2",
+              "baseline",
+              "original",
+             ]
 
-model.add(Conv2D(16, (3, 3), strides=(1, 1), padding='same',
-                 activation='relu'))
-model.add(MaxPooling2D((3, 3), strides=(1, 1), padding='same'))
+if MODEL == "":
+    SETTING = model_list[SETTING]
+else:
+    SETTING = MODEL
 
-model.add(Flatten())
-model.add(Dense(64, activation='relu'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(nb_classes, activation='softmax'))
+setting = model_list.index(SETTING)
 
-model.summary()
+def build_model(setting):
+    print ("[model]: " + model_list[setting])
+
+    model = Sequential()
+    model.add(Conv2D(16, (3, 3), strides=(1, 1), padding='same',
+                     activation='relu', input_shape=(208,208,3)))
+    model.add(MaxPooling2D((3,3), (2, 2), padding='same'))
+    # model.add(BatchNormalization())
+    model.add(LRN2D())
+
+    model.add(Conv2D(16, (3, 3), strides=(1, 1), padding='same',
+                     activation='relu'))
+    model.add(MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
+    # model.add(BatchNormalization())
+    model.add(LRN2D())
+    if setting == 9:
+        # [orginal model]
+        model.pop()
+        model.pop()
+        model.add(MaxPooling2D((3, 3), strides=(1, 1), padding='same'))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(128, activation='relu'))
+    elif setting == 8:
+        # [baseline model]
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(128, activation='relu'))
+    elif setting == 7:
+        # [io-units_x1+units_x2 model]
+        model.add(Flatten())
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(512, activation='relu'))
+    elif setting == 6:
+        # [io-units_x1+units_x1 model]
+        model.add(Flatten())
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(256, activation='relu'))
+    elif setting == 5:
+        # [io-units_x1+layer model]
+        model.add(Flatten())
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(64, activation='relu'))
+    elif setting == 4:
+        # [io-units_x1 model]
+        model.add(Flatten())
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(128, activation='relu'))
+    elif setting == 3:
+        # [io-layer model]
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+    elif setting == 2:
+        # [io-units_x2]
+        model.add(Flatten())
+        model.add(Dense(32, activation='relu'))
+        model.add(Dense(128, activation='relu'))
+    elif setting == 1:
+        # [io-units_x1+cnn_pooling]
+        model.add(Conv2D(16, (3, 3), strides=(1, 1), padding='same', activation='relu'))
+        model.add(MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
+        # model.add(BatchNormalization())
+        model.add(LRN2D())
+
+        model.add(Flatten())
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(128, activation='relu'))
+    else:
+        model.add(Conv2D(16, (3, 3), strides=(1, 1), padding='same', activation='relu'))
+        model.add(MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
+        # model.add(BatchNormalization())
+        model.add(LRN2D())
+
+        model.add(Conv2D(16, (3, 3), strides=(1, 1), padding='same', activation='relu'))
+        model.add(MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
+        # model.add(BatchNormalization())
+        model.add(LRN2D())
+
+        model.add(Flatten())
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(128, activation='relu'))
+
+    model.add(Dense(nb_classes, activation='softmax'))
+    return model
+
+def save_summary(model, header, suffix):
+    assert(suffix.split(".")[0] == "")
+    with open(file_header + suffix,'w') as fh:
+        # Pass the file handle in as a lambda function to make it callable
+        model.summary(print_fn=lambda x: fh.write(x + '\n'))
+
+
+from keras.utils import plot_model #, print_summary
+if ALL:
+    for setting in range(len(model_list)):
+        model = build_model(setting)
+        description = model_list[setting]
+        file_header = os.path.join(SAVE_DIR, "model_" + str(model.count_params()) + "_" + \
+                                description)
+        plot_model(model, to_file=file_header+".pdf", show_shapes=True)
+        save_summary(model, file_header, ".txt")
+else:
+    model = build_model(setting)
+    file_header = os.path.join(SAVE_DIR, "model_" + str(model.count_params()) + "_" + \
+                             SETTING)
+    plot_model(model, to_file=file_header + ".pdf", show_shapes=True)
+    save_summary(model, file_header, ".txt")
+
+# model.summary()
+
 
 # model.compile(loss='categorical_crossentropy',
-#               optimizer='sgd',
+#               optimizer='adam',
 #               metrics=['accuracy'])
---
+++ train_paras.py
@@ -0,0 +1,250 @@
+import os
+import numpy as np
+import tensorflow as tf
+import provider
+import model_paras as model
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--quality', type=int, default=0, help='Image quality [default: 0]')
+parser.add_argument('--setting', type=int, default=8, help='Model architecture (0-8) [default: 8]')
+parser.add_argument('--model', type=str, default="", help='Model architecture description (0-8) [default: ""]')
+parser.add_argument('--batch_size', type=int, default=128, help='Batch size during training [default: 128]')
+parser.add_argument('--num_epoch', type=int, default=100, help='Batch size during training [default: 100]')
+parser.add_argument('--learning_rate', type=float, default=0.0001, help='Initial learning rate [default: 0.0001]')
+# parser.add_argument('--max_step', type=int, default=20000, help='Max training step [default: 0.0001]')
+FLAGS = parser.parse_args()
+
+# MAX_STEP = FLAGS.max_step
+MAX_EPOCH = FLAGS.num_epoch
+learning_rate = FLAGS.learning_rate
+BATCH_SIZE = FLAGS.batch_size
+QUALITY = FLAGS.quality
+SETTING = FLAGS.setting
+MODEL = FLAGS.model
+
+model_list = ["io-units_x1+cnn_pooling2",
+              "io-units_x1+cnn_pooling",
+              "io-units_x2",
+              "io-layer",
+              "io-units_x1",
+              "io-units_x1+layer",
+              "io-units_x1+units_x1",
+              "io-units_x1+units_x2",
+              "baseline",
+              "original"
+             ]
+
+if MODEL == "":
+    SETTING = model_list[SETTING]
+else:
+    SETTING = MODEL
+
+print ("model: " + SETTING)
+
+N_CLASSES = 2
+IMG_W = 208  # resize the image, if the input image is too large, training will be very slow.
+IMG_H = 208
+RATIO = 0.4 # take 20% of dataset as validation data
+# BATCH_SIZE = 128
+CAPACITY = 20000
+# MAX_STEP = 20000 # with current parameters, it is suggested to use MAX_STEP>10k
+# learning_rate = 0.0001 # with current parameters, it is suggested to use learning rate<0.0001
+
+train_dir = "data/quality_" + str(QUALITY) + "/"
+logs_dir = "logs/" + str(SETTING) + "/quality_" + str(QUALITY) + "/"
+logs_train_dir = "logs/" + str(SETTING) + "/quality_" + str(QUALITY) + "/" + "train/"
+logs_val_dir = "logs/" + str(SETTING) + "/quality_" + str(QUALITY) + "/" + "val/"
+
+print ("train_dir: " + train_dir)
+print ("logs_train_dir: " + logs_train_dir)
+print ("logs_val_dir: " + logs_val_dir)
+
+assert (os.path.exists(train_dir))
+if not os.path.exists(logs_train_dir):
+    os.makedirs(logs_train_dir)
+if not os.path.exists(logs_val_dir):
+    os.makedirs(logs_val_dir)
+
+os.system('cp %s %s' % ("model_paras.py", logs_dir)) # bkp of model def
+os.system('cp train.py %s' % (logs_dir)) # bkp of train procedure
+
+LOG_FOUT = open(os.path.join(logs_train_dir, 'log_train.txt'), 'w')
+LOG_FOUT.write(str(FLAGS)+'\n')
+
+# train_dir = 'data/train/'
+# logs_train_dir = 'logs/original/train/'
+# logs_val_dir = 'logs/orginal/val/'
+
+def log_string(out_str):
+    LOG_FOUT.write(out_str+'\n')
+    LOG_FOUT.flush()
+    print(out_str)
+
+def run_training(train_dir, logs_train_dir, logs_val_dir):
+
+    # train, train_label, val, val_label = provider.get_files(train_dir, RATIO)
+    '''
+    train_batch, train_label_batch = provider.get_batch(train,
+                                                  train_label,
+                                                  IMG_W,
+                                                  IMG_H,
+                                                  BATCH_SIZE,
+                                                  CAPACITY)
+    val_batch, val_label_batch = provider.get_batch(val,
+                                                  val_label,
+                                                  IMG_W,
+                                                  IMG_H,
+                                                  BATCH_SIZE,
+                                                  CAPACITY)
+    '''
+    x = tf.placeholder(tf.float32, shape=[BATCH_SIZE, IMG_W, IMG_H, 3])
+    y_ = tf.placeholder(tf.int32, shape=[BATCH_SIZE])
+
+    batch = tf.Variable(0, name='global_step', trainable=False)
+
+    logits = model.inference(x, BATCH_SIZE, N_CLASSES, setting=SETTING)
+    print logits
+    loss = model.loss(logits, y_)
+    print loss
+    # acc = model.evaluation(logits, y_)
+    train_op = model.training(loss, learning_rate, batch)
+
+    print "[Loading data ...]"
+    data, label = provider.load_data(train_dir)
+    data, label, _ = provider.shuffle_data(data, label)
+    print "[Finish Loading]"
+
+    file_size = len(label)
+    val_data, val_label = data[int((1-RATIO)*file_size):, ...], label[int((1-RATIO)*file_size):]
+    train_data, train_label = data[0:int((1-RATIO)*file_size), ...], label[0:int((1-RATIO)*file_size)]
+    print ("train_data", train_data.shape, train_label.shape)
+    print ("val_data", val_data.shape, val_label.shape)
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+
+    with tf.Session(config=config) as sess:
+        saver = tf.train.Saver()
+        sess.run(tf.global_variables_initializer())
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess= sess, coord=coord)
+
+        summary_op = tf.summary.merge_all()
+        train_writer = tf.summary.FileWriter(logs_train_dir, sess.graph)
+        val_writer = tf.summary.FileWriter(logs_val_dir, sess.graph)
+
+        ops = {'images_pl': x,
+               'labels_pl': y_,
+               'logits': logits,
+               'loss': loss,
+               'train_op': train_op,
+               'merged': summary_op,
+               'step': batch}
+
+        try:
+            for epoch in np.arange(MAX_EPOCH):
+                if coord.should_stop():
+                        break
+
+                log_string('**** EPOCH %03d ****' % (epoch))
+                sys.stdout.flush()
+
+                train_one_epoch(sess, ops, train_writer, train_data, train_label)
+                eval_one_epoch(sess, ops, val_writer, val_data, val_label)
+
+                # Save the variables to disk.
+                if epoch % 10 == 0:
+                    save_path = saver.save(sess, os.path.join(logs_dir, "model.ckpt"))
+                    log_string("Model saved in file: %s" % save_path)
+
+        except tf.errors.OutOfRangeError:
+            print('Done training -- epoch limit reached')
+        finally:
+            coord.request_stop()
+        coord.join(threads)
+
+def train_one_epoch(sess, ops, train_writer, current_data, current_label):
+    """ ops: dict mapping from string to tf ops """
+    # current_data, current_label = provider.load_data(train_dir)
+    # file_size = len(current_label)
+    # current_data, current_label = current_data[0:int((1-RATIO)*file_size), ...],  current_label[0:int((1-RATIO)*file_size)]
+    # current_data, current_label, _ = provider.shuffle_data(current_data, current_label)
+    # print _
+    # print (current_data.shape, current_label.shape)
+
+    train_size = len(current_label)
+    num_batches = train_size // BATCH_SIZE
+
+    total_correct = 0
+    total_seen = 0
+    loss_sum = 0
+
+    for batch_idx in range(num_batches):
+        start_idx = batch_idx * BATCH_SIZE
+        end_idx = (batch_idx+1) * BATCH_SIZE
+
+        feed_dict = {ops['images_pl']: current_data[start_idx:end_idx,...],
+                        ops['labels_pl']: current_label[start_idx:end_idx]}
+        summary, step, _, loss_val, pred_val = sess.run([ops['merged'], ops['step'],
+            ops['train_op'], ops['loss'], ops['logits']], feed_dict=feed_dict)
+        train_writer.add_summary(summary, step)
+        # print pred_val, loss_val
+        pred_val = np.argmax(pred_val, 1)
+        # print pred_val, current_label[start_idx:end_idx]
+        correct = np.sum(pred_val == current_label[start_idx:end_idx])
+        # print correct
+        total_correct += correct
+        total_seen += BATCH_SIZE
+        loss_sum += loss_val
+        # print (total_correct / float(total_seen))
+
+    # print (loss_sum, num_batches)
+    # print (total_correct, total_seen)
+    log_string('mean loss: %f' % (loss_sum / float(num_batches)))
+    log_string('accuracy: %f' % (total_correct / float(total_seen)))
+
+def eval_one_epoch(sess, ops, val_writer, current_data, current_label):
+    """ ops: dict mapping from string to tf ops """
+    total_correct = 0
+    total_seen = 0
+    loss_sum = 0
+    total_seen_class = [0 for _ in range(N_CLASSES)]
+    total_correct_class = [0 for _ in range(N_CLASSES)]
+
+    # current_data, current_label = provider.load_data(train_dir)
+    # file_size = len(current_label)
+    # current_data, current_label = current_data[int((1-RATIO)*file_size):, ...],  current_label[int((1-RATIO)*file_size):]
+    # current_data, current_label, _ = provider.shuffle_data(current_data, current_label)
+    # print _
+    # print (current_data.shape, current_label.shape)
+
+    val_size = current_label.shape[0]
+    num_batches = val_size // BATCH_SIZE
+
+    for batch_idx in range(num_batches):
+        start_idx = batch_idx * BATCH_SIZE
+        end_idx = (batch_idx+1) * BATCH_SIZE
+
+        feed_dict = {ops['images_pl']: current_data[start_idx:end_idx, ...],
+                        ops['labels_pl']: current_label[start_idx:end_idx]}
+        summary, step, loss_val, pred_val = sess.run([ops['merged'], ops['step'],
+            ops['loss'], ops['logits']], feed_dict=feed_dict)
+        val_writer.add_summary(summary, step)
+        pred_val = np.argmax(pred_val, 1)
+        correct = np.sum(pred_val == current_label[start_idx:end_idx])
+        total_correct += correct
+        total_seen += BATCH_SIZE
+        loss_sum += (loss_val*BATCH_SIZE)
+        for i in range(start_idx, end_idx):
+            l = current_label[i]
+            total_seen_class[l] += 1
+            total_correct_class[l] += (pred_val[i-start_idx] == l)
+
+    log_string('eval mean loss: %f' % (loss_sum / float(total_seen)))
+    log_string('eval accuracy: %f'% (total_correct / float(total_seen)))
+    log_string('eval avg class acc: %f' % (np.mean(np.array(total_correct_class) / np.array(total_seen_class,dtype=np.float))))
+
+if __name__ == "__main__":
+    run_training(train_dir, logs_train_dir, logs_val_dir)
